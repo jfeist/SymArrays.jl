19dc9ea372d385c4c8680d604d962b8ecba80a0c
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-17 11:17:10 +0000

    build based on 56b4677

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#SymArrays.jl-1","page":"Home","title":"SymArrays.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [SymArrays]","category":"page"}]
+[{"location":"#SymArrays.jl-1","page":"Home","title":"SymArrays.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [SymArrays]","category":"page"},{"location":"#SymArrays.SymArr_ifsym-Tuple{Any,Any}","page":"Home","title":"SymArrays.SymArr_ifsym","text":"SymArr_ifsym(A,Nsyms) make a SymArray if there is some symmetry (i.e., any of the Nsyms are not 1)\n\n\n\n\n\n","category":"method"}]
 }
